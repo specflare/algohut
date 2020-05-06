604f2b9b902d8279cfc1416defb24853b41b6218
Liviu Gheorghisan <petascalr@gmail.com>
2020-05-06 22:59:50 +0300

    Added suffix tree implementation

--- src/test/java/com/github/petascalr/algohut/trees/SuffixTreeTest.java
+++ src/test/java/com/github/petascalr/algohut/trees/SuffixTreeTest.java
@@ -1,4 +1,20 @@
 package com.github.petascalr.algohut.trees;
 
+import org.junit.Test;
+
 public class SuffixTreeTest {
+
+    @Test
+    public void basicTest() {
+        String str = "HAVANABANANA";
+
+        SuffixTree st = new SuffixTree();
+        st.T = str.toCharArray();
+        st.N = st.T.length - 1;
+
+        for (int i = 0; i <= st.N; i++)
+            st.AddPrefix(st.active, i);
+
+        st.dump_edges(st.N);
+    }
 }
